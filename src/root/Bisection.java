f0ac109f64a18fb7e5e02faa59bc3a044aa1e891
Mahdi Jamil <134617316+Mahdi-jamil@users.noreply.github.com>
2024-06-11 14:13:31 -0700

    Update Bisection.java

--- src/root/Bisection.java
+++ src/root/Bisection.java
@@ -5,28 +5,36 @@ import static root.Functions.fx;
 /***
  * @author Mahdi Jamil
  */
-public class Bisection {
+public class bisection {
 
-    public static double bisection(double left, double right, double eps) {
-        if (fx(left) * fx(right) >= 0) {
-            System.out.println("You have not assumed right left and right ");
-            return -1;
-        }
+    public static double fx(double x) {
+        return Math.exp(x) - Math.pow((2 - x), 3);
+    }
 
-        double middle = (left + right) / 2;
+    public static double dfx(double x) {
+        return Math.exp(x) + 3 * Math.pow(2 - x, 2);
+    }
+    public static double bisection(double left, double right, double eps) {
+        if (fx(left) * fx(right) >= 0) {
+            System.out.println("You have not assumed right left and right ");
+            return -1;
+        }
+        
+        int N = (int)Math.floor((Math.log(right-left)-Math.log(eps))/Math.log(2));
+        int i=0;
+        
+        double middle = (left + right) / 2;
 
-        while (Math.abs(fx(middle)) > eps) {
-            middle = (left + right) / 2;
+        while (Math.abs(fx(middle)) > eps && i<=N) {
+            middle = (left + right) / 2;
 
-            if (fx(middle) == 0) break;
+            if (fx(middle) == 0) break;
 
-            right = (fx(left) * fx(middle) < 0) ? middle : right;
-            left = (fx(left) * fx(middle) < 0) ? left : middle;
+            right = (fx(left) * fx(middle) < 0) ? middle : right;
+            left = (fx(left) * fx(middle) < 0) ? left : middle;
+            i++;
 
-        }
-        return middle;
-    }
-
-
-
+        }
+        return middle;
+    }
 }
